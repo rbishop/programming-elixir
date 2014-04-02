38848df0ae216a8ab0082ded379f8d77490dbeda
Richard Bishop <rbishop87@gmail.com>
2014-04-02 11:15:37 -0600

    Implement sum and mapsum

--- reducer.exs
+++ reducer.exs
@@ -1,8 +1,12 @@
 defmodule Reducer do
-  def sum(list),      do: _reduce(list, &(&1 + &2))
-  def multiply(list), do: _reduce(list, &(&1 * &2))
-  def max(list),      do: _reduce(list, &(max(&1, &2)))
-  def min(list),      do: _reduce(list, &(min(&1, &2)))
+  def sum(list),          do: _reduce(list, &(&1 + &2))
+  def multiply(list),     do: _reduce(list, &(&1 * &2))
+  def max(list),          do: _reduce(list, &(max(&1, &2)))
+  def min(list),          do: _reduce(list, &(min(&1, &2)))
+  def mapsum(list, func), do: sum(_map(list, func))
+
+  defp _map([head | []], func),   do: [func.(head)]
+  defp _map([head | tail], func), do: [func.(head)] ++ _map(tail, func)
   
   defp _reduce([], _func),                    do: []
   defp _reduce([head | []], _func),           do: head
