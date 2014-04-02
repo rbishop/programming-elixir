f1b858beb413b4f13a40eb5ecb8c1659ec07b826
Richard Bishop <rbishop87@gmail.com>
2014-04-02 10:38:32 -0600

    Implement min and max using reduce

--- reducer.exs
+++ reducer.exs
@@ -1,8 +1,10 @@
 defmodule Reducer do
   def sum(list),      do: _reduce(list, &(&1 + &2))
   def multiply(list), do: _reduce(list, &(&1 * &2))
+  def max(list),      do: _reduce(list, &(max(&1, &2)))
+  def min(list),      do: _reduce(list, &(min(&1, &2)))
   
-  defp _reduce([], _),                        do: []
+  defp _reduce([], _func),                    do: []
   defp _reduce([head | []], _func),           do: head
   defp _reduce([head | [next | tail]], func), do: _reduce([func.(head, next)] ++ tail, func)
 end
